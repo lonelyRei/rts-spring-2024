ee64e86d673fab87acab310ba97d64f4a719f867
d.shepovalov <shepovalovda@gmail.com>
2024-03-24 21:55:42 +0400

    Complete second task

--- tasks/task02/task02.ino
+++ tasks/task02/task02.ino
@@ -1,7 +1,31 @@
-void setup() {
+#include <TaskManagerIO.h>
 
+long times[5] = {10000, 1000, 500, 100, 50};
+int* lamps = new int[5];
+int pins[5] = {3, 5, 6, 9, 10};
+
+void toggleLamp(int index) {
+  if (lamps[index] == LOW) {
+    lamps[index] = HIGH;
+  } else {
+    lamps[index] = LOW;
+  }
+  digitalWrite(pins[index], lamps[index]);
+}
+
+void setup() {
+  for (int i = 0; i < 5; i++) {
+    lamps[i] = LOW;
+    pinMode(pins[i], OUTPUT);
+  }
+
+  for (int i = 0; i < 5; i++) {
+    taskid_t taskId = taskManager.scheduleFixedRate(times[i], [i] {
+      toggleLamp(i);
+    }, TIME_MICROS);
+  }
 }
 
 void loop() {
-
+  taskManager.runLoop();
 }
